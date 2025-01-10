7fc0bc3663a53111b59ff32f465cc67c1bf03794
Nurlytan <nurik.bro.2000@mail.ru>
2025-01-11 02:10:50 +0500

    Model set up

--- file-service/app/Models/File.php
+++ file-service/app/Models/File.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model
 {
     use HasFactory;
+
+     /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'location', 'extension', 'size', 'preview'];
 }
