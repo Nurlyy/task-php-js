fc1c4e98c430b9436776d173c38bbfd5cd69b968
Nurlytan <nurik.bro.2000@mail.ru>
2025-01-12 03:41:51 +0500

    set up frontend vite, tailwind vue inertia

---
+++ file-service/resources/js/Pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold">Welcome to Laravel Inertia</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+};
+</script>
--- file-service/vite.config.js
+++ file-service/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import DefineOptions from 'unplugin-vue-define-options/vite'
 
 export default defineConfig({
     plugins: [
-        vue(),
-        DefineOptions(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
 });
