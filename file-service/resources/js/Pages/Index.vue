77df93d428d564832edf85e7dc45c1af7fdf030e
Nurlytan <nurik.bro.2000@mail.ru>
2025-01-12 03:47:43 +0500

    set up axios

--- file-service/resources/js/Pages/Index.vue
+++ file-service/resources/js/Pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="text-3xl font-bold">Welcome to Laravel Inertia</h1>
+        <h1 class="text-3xl font-bold underline bg-blue-50">Welcome to Laravel Inertia</h1>
     </div>
 </template>
 
